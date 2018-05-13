0d4dda3d73b16a8ba5048056face2cef30530776
Hidran Arias <hidran@gmail.com>
2018-05-13 20:27:31 +0200

    add sidemenu app

---
+++ sidemenu/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import {Component, ViewChild} from '@angular/core';
+import {MenuController, NavController, Platform} from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+import {AboutPage} from "../pages/about/about";
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = HomePage;
+  aboutPage: any = 'AboutPage'
+   settingsPage: any = 'SettingsPage'
+  @ViewChild('myNav') navCtrl: NavController
+
+  constructor(platform: Platform,
+              statusBar: StatusBar, splashScreen: SplashScreen,
+              private menuCtrl : MenuController
+
+  ) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
+  openPage( page: any) {
+    this.navCtrl.setRoot(page);
+    this.menuCtrl.close();
+  }
+}
+
